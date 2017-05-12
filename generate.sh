39317fd439c80142b8d4c2cd5e128f35eb1a1d79
shivasdst <shivably04sdst@gmail.com>
2017-05-12 18:28:19 +0530

    updated file

--- generate.sh
+++ generate.sh
@@ -7,5 +7,7 @@ pwd="mysql"
 
 echo "drop database $db; create database $db charset utf8 collate utf8_general_ci;" | /usr/bin/mysql -uroot -pmysql
 
+echo "Media Insertion begins";
 perl insert_media.pl $host $db $usr $pwd
+echo "Photo Insertion begins";
 perl insert_photo.pl $host $db $usr $pwd
